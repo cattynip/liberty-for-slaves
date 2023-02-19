a0858c5f3475e65d15acbd8fadc15afe37f70bd8
Cattynip <cattynip.cattynip@gmail.com>
2023-02-19 12:32:53 -0800

    feat(config): ready to develop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="This is a website for infroming about Black Slave Trading."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const options = {
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = options;
